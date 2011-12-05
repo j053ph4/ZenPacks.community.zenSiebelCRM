cf23ffdadf6022088382a13b23f2862d852c3c49
Joseph Anderson <janderson@atxg.com>
2011-12-05 15:30:02 -0600

    added zProperties for zensiebelperf

--- ZenPacks/community/zenSiebelCRM/__init__.py
+++ ZenPacks/community/zenSiebelCRM/__init__.py
@@ -1,34 +1,21 @@
-# Nothing is required in this __init__.py, but it is an excellent place to do
-# many things in a ZenPack.
-#
-# The example below which is commented out by default creates a custom subclass
-# of the ZenPack class. This allows you to define custom installation and
-# removal routines for your ZenPack. If you don't need this kind of flexibility
-# you should leave the section commented out and let the standard ZenPack
-# class be used.
-#
-# Code included in the global scope of this file will be executed at startup
-# in any Zope client. This includes Zope itself (the web interface) and zenhub.
-# This makes this the perfect place to alter lower-level stock behavior
-# through monkey-patching.
-
 import Globals
 from Products.ZenModel.ZenPack import ZenPack as ZenPackBase
-from Products.ZenUtils.Utils import unused
+from Products.ZenUtils.Utils import zenPath,unused
 import os
 
 unused(Globals)
 
 
 class ZenPack(ZenPackBase):
-    # All zProperties defined here will automatically be created when the
-    # ZenPack is installed.
     packZProperties = [
         ('zSiebelGateway', 'default value', 'string'),
         ('zSiebelEnterprise', 'default value', 'string'),
         ('zSiebelServer', 'default value', 'string'),
         ('zSiebelUser', 'SADMIN', 'string'),
         ('zSiebelPassword', 'sadmin', 'password'),
+        ('zSiebelPerfCycleSeconds', 300, 'int'),
+        ('zSiebelPerfCyclesPerConnection', 5, 'int'),
+        ('zSiebelPerfTimeoutSeconds', 10, 'int'),
         ]
 
     def symlinkScript(self):

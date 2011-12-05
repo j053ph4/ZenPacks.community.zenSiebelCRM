c8efa26fd769dc51ab14b600d5bb1969ce44b03d
Joseph Anderson <janderson@atxg.com>
2011-12-05 15:28:29 -0600

    support added for new datasources

--- ZenPacks/community/zenSiebelCRM/info.py
+++ ZenPacks/community/zenSiebelCRM/info.py
@@ -1,21 +1,10 @@
-# This file is the conventional place for "Info" adapters. Info adapters are
-# a crucial part of the Zenoss API and therefore the web interface for any
-# custom classes delivered by your ZenPack. Examples of custom classes that
-# will almost certainly need info adapters include datasources, custom device
-# classes and custom device component classes.
-
-# Mappings of interfaces (interfaces.py) to concrete classes and the factory
-# (these info adapter classes) used to create info objects for them are managed
-# in the configure.zcml file.
-
 from zope.component import adapts
 from zope.interface import implements
-
 from Products.Zuul.infos import ProxyProperty
 from Products.Zuul.infos.component import ComponentInfo
-
+from Products.Zuul.infos import InfoBase
 from ZenPacks.community.zenSiebelCRM.SiebelComponent import SiebelComponent
-from ZenPacks.community.zenSiebelCRM.interfaces import ISiebelComponentInfo
+from ZenPacks.community.zenSiebelCRM.interfaces import *
 
 
 class SiebelComponentInfo(ComponentInfo):
@@ -27,4 +16,78 @@ class SiebelComponentInfo(ComponentInfo):
     startTime = ProxyProperty("startTime")
     endTime = ProxyProperty("endTime")
 
+class SiebelPerfDataSourceInfo(InfoBase):
+    implements(ISiebelPerfDataSourceInfo)
+
+    @property
+    def id(self):
+        return '/'.join( self._object.getPrimaryPath() )
+
+    @property
+    def source(self):
+        return self._object.getDescription()
+
+    @property
+    def type(self):
+        return self._object.sourcetype
+
+    command = ProxyProperty('command')
+    enabled = ProxyProperty('enabled')
+
+    @property
+    def testable(self):
+        """
+        We can test this datasource against a specific siebel device
+        """
+        return True
+
+class SiebelTasksDataSourceInfo(InfoBase):
+    implements(ISiebelTasksDataSourceInfo)
+
+    @property
+    def id(self):
+        return '/'.join( self._object.getPrimaryPath() )
+
+    @property
+    def source(self):
+        return self._object.getDescription()
+
+    @property
+    def type(self):
+        return self._object.sourcetype
+
+    command = ProxyProperty('command')
+    enabled = ProxyProperty('enabled')
+
+    @property
+    def testable(self):
+        """
+        We can test this datasource against a specific siebel device
+        """
+        return True
+    
+class SiebelStatusDataSourceInfo(InfoBase):
+    implements(ISiebelStatusDataSourceInfo)
+
+    @property
+    def id(self):
+        return '/'.join( self._object.getPrimaryPath() )
+
+    @property
+    def source(self):
+        return self._object.getDescription()
+
+    @property
+    def type(self):
+        return self._object.sourcetype
+
+    command = ProxyProperty('command')
+    enabled = ProxyProperty('enabled')
+
+    @property
+    def testable(self):
+        """
+        We can test this datasource against a specific siebel device
+        """
+        return True
 
--- ZenPacks/community/zenSiebelCRM/interfaces.py
+++ ZenPacks/community/zenSiebelCRM/interfaces.py
@@ -1,5 +1,6 @@
 from Products.Zuul.form import schema
 from Products.Zuul.interfaces.component import IComponentInfo
+from Products.Zuul.interfaces import IInfo
 from Products.Zuul.utils import ZuulMessageFactory as _t
 
 
@@ -9,4 +10,37 @@ class ISiebelComponentInfo(IComponentInfo):
     CCalias = schema.Text(title=_t(u"CCalias"),group='Details')
     startTime = schema.Text(title=_t(u"startTime"),group='Details')
     endTime = schema.Text(title=_t(u"endTime"),group='Details')
+
+
+class ISiebelPerfDataSourceInfo(IInfo):
+    name = schema.Text(title=_t(u"Name"),
+                       xtype="idfield",
+                       description=_t(u"The name of this datasource"))
+    type = schema.Text(title=_t(u"Type"),
+                       readonly=True)
+    command = schema.Text(title=_t(u"Command"),
+                          description=_t(u"Example: list servers show SBLSRVR_NAME,HOST_NAME,SBLSRVR_STATE,START_TIME,END_TIME "))
+    enabled = schema.Bool(title=_t(u"Enabled"))
+
+class ISiebelTasksDataSourceInfo(IInfo):
+    name = schema.Text(title=_t(u"Name"),
+                       xtype="idfield",
+                       description=_t(u"The name of this datasource"))
+    type = schema.Text(title=_t(u"Type"),
+                       readonly=True)
+    command = schema.Text(title=_t(u"Command"),
+                          description=_t(u"Example: list servers show SBLSRVR_NAME,HOST_NAME,SBLSRVR_STATE,START_TIME,END_TIME "),
+                          readonly=True)
+    enabled = schema.Bool(title=_t(u"Enabled"))
+
+class ISiebelStatusDataSourceInfo(IInfo):
+    name = schema.Text(title=_t(u"Name"),
+                       xtype="idfield",
+                       description=_t(u"The name of this datasource"))
+    type = schema.Text(title=_t(u"Type"),
+                       readonly=True)
+    command = schema.Text(title=_t(u"Command"),
+                          description=_t(u"Example: list servers show SBLSRVR_NAME,HOST_NAME,SBLSRVR_STATE,START_TIME,END_TIME "),
+                          readonly=True)
+    enabled = schema.Bool(title=_t(u"Enabled"))
 
